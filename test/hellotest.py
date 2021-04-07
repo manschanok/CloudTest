b2fd54128243d3cd3b7e7ceb52f888aaf9e673e9
manschanok <79188292+manschanok@users.noreply.github.com>
2021-04-07 15:35:10 +0700

    Update hellotest.py

--- test/hellotest.py
+++ test/hellotest.py
@@ -4,9 +4,9 @@ app = Flask(_name_)
 
 @app.route('/')
 def Home():
-  return "มนัสชนก เพ็งมีศรี 6006021612069"
+return "มนัสชนก เพ็งมีศรี 6006021612069"
 
 
 if _name_ == '_main_':
- app.debug = True
- app.run(host='0.0.0.0', port=80)
+app.debug = True
+app.run(host='0.0.0.0', port=80)
